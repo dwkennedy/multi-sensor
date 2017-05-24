1c4feb32031a2e029a5c78dd94d6b42389c44121
dwkennedy <github@kennedygang.com>
2017-05-24 19:40:19 +0000

    Add files via upload
    Added heading, roll, pitch

--- webserver.py
+++ webserver.py
@@ -13,6 +13,9 @@ press = -999.0
 RH = -999.0
 wind_spd = -999.0
 wind_dir = -999.0
+heading = -999.0
+roll = -999.0
+pitch = -999.0
 
 class MyHTTPRequestHandler(http.server.BaseHTTPRequestHandler):
     def do_GET(self):
@@ -25,7 +28,8 @@ class MyHTTPRequestHandler(http.server.BaseHTTPRequestHandler):
             string = f.read()
             self.wfile.write(string.encode('utf-8'))
         elif self.path == '/data':
-            string = json.dumps({"temp": temp, "press": press, "RH": RH, "wind_spd": wind_spd, "wind_dir": wind_dir})
+            string = json.dumps({"temp": temp, "press": press, "RH": RH, "wind_spd": wind_spd, "wind_dir": wind_dir,
+                                 "heading": heading, "roll": roll, "pitch": pitch})
             self.wfile.write(string.encode('utf-8'))
             #self.wfile.write(self.path.encode('utf-8'))
         elif self.path == '/favicon.ico':
@@ -57,7 +61,7 @@ class Thread_B(threading.Thread):
         self.name = name
 
     def run(self):
-        global temp, press, RH, wind_spd, wind_dir
+        global temp, press, RH, wind_spd, wind_dir, heading, roll, pitch
         filename = time.strftime("surface_data_%Y%m%d.sfc")
         print("Opening new file: ", filename)
         file = open(filename, 'a')
@@ -87,7 +91,7 @@ class Thread_B(threading.Thread):
                             print("\nWind Direction (deg): " + wind_dir + "  Wind Speed (m/s): " + wind_spd
                                   + "  Data Status (A=valid): " + msg.data[4])
                 except:
-                    msg = pynmea2.parse(line)
+                    msg = pynmea2.parse(x.decode("utf-8"))
                     pass
 
                 try:
@@ -102,20 +106,34 @@ class Thread_B(threading.Thread):
                                 print("\nTemp (C): " + temp + "  RH (%): " + RH + "  Pressure (hPa): " +
                                       press)
                 except:
-                    msg = pynmea2.parse(line)
+                    msg = pynmea2.parse(x.decode("utf-8"))
                     pass
 
-                try:
-                    if msg.talker == 'GP':  # decode GPRMC and GPGGA here.  update time/date variables
-                        pass
-                except:
-                    pass
-
+                #print (msg.talker)
                 print(x.decode("utf-8"), end="")
                 file.write(x.decode("utf-8") + '\n')
 
             while (gps.in_waiting):
                 x = gps.readline()
+                try:
+                    msg = pynmea2.parse(x.decode("utf-8"))
+                    #print(msg.talker)
+                    if msg.manufacturer == 'CLM':
+                        roll = msg.data[1]
+                        pitch = msg.data[2]
+                        heading = msg.data[3]
+                        print("\nHeading (mag): " + heading + " Roll: " + roll + " Pitch: " + pitch)
+                except:
+                    #print("error")
+                    pass
+
+                try:
+                    msg = pynmea2.parse(x.decode("utf-8"))
+                    if msg.talker == 'GP':  # decode GPRMC and GPGGA here.  update time/date variables
+                        pass
+                except:
+                    pass
+
                 print(x.decode("utf-8"), end="")
                 file.write(x.decode("utf-8") + '\n')
 
