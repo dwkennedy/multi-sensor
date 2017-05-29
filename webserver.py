780828e5ba68179b11ae0214e01727ac81c17218
Doug Kennedy <github@kennedygang.com>
2017-05-29 03:24:55 +0000

    Add files via upload
    Added check for missing WXT-536 data.  indicated -999 for missing values
    Drop SASSI .mmm file into SASSI outgoing directory, c:\users\fofs\appdata\local\rasmussen systems\sassi\outgoing
    Web page now has google maps navigated to vehicle location w/ marker displayed
    Font size of surface data display embiggened
    now makes extensive use of javascript callbacks and functional programming

--- webserver.py
+++ webserver.py
@@ -6,6 +6,7 @@ import time
 import sys
 import serial
 import pynmea2
+import random
 
 PORT = 8000
 temp = -999.0
@@ -16,26 +17,64 @@ wind_dir = -999.0
 heading = -999.0
 roll = -999.0
 pitch = -999.0
+lat = -999.0
+lon = -999.0
+course = -999.0
+spd = -999.0
+alt = -999.0
+
 
 class MyHTTPRequestHandler(http.server.BaseHTTPRequestHandler):
     def do_GET(self):
         self.send_response(200)
-        self.send_header('Content-type','text/html')
-        self.end_headers()
-        self.flush_headers()
+        #self.send_header('Content-type','text/html')
+        #self.end_headers()
+        #self.flush_headers()
         if self.path == '/':
             f = open("sfc_template.html")
             string = f.read()
+            self.send_header('Content-type', 'text/html')
+            self.end_headers()
+            self.flush_headers()
             self.wfile.write(string.encode('utf-8'))
         elif self.path == '/data':
             string = json.dumps({"temp": temp, "press": press, "RH": RH, "wind_spd": wind_spd, "wind_dir": wind_dir,
-                                 "heading": heading, "roll": roll, "pitch": pitch})
+                                 "heading": heading, "roll": roll, "pitch": pitch, "lat":lat, "lon":lon, "course":course,
+                                 "spd":spd, "alt":alt})
+            self.send_header('Content-type', 'application/json')
+            self.end_headers()
+            self.flush_headers()
             self.wfile.write(string.encode('utf-8'))
             #self.wfile.write(self.path.encode('utf-8'))
         elif self.path == '/favicon.ico':
             f = open("favicon.ico", 'rb')
             string = f.read()
+            self.send_header('Content-type', 'image/png')
+            self.end_headers()
+            self.flush_headers()
             self.wfile.write(string)
+        elif self.path == '/loc.kml':
+            string = ('<?xml version="1.0" encoding="UTF-8"?>'
+                      '<kml xmlns="http://www.opengis.net/kml/2.2">'
+                      '<Placemark>'
+                      '<name>CLAMPS2</name>'
+                      '<LookAt>'
+                      '<longitude>%f</longitude>'
+                      '<latitude>%f</latitude>'
+                      '<range>5000</range>'
+                      '<tilt>0</tilt>'
+                      '</LookAt>'
+                      '<Point>'
+                      '<coordinates> %f,%f </coordinates>'
+                      '</Point>'
+                      '</Placemark>'
+                      '</kml>'
+                      ) % (lon, lat, lon, lat)
+            self.send_header('Content-type', 'application/vnd.google-earth.kml+xml')
+            self.end_headers()
+            self.flush_headers()
+            self.wfile.write(string.encode('utf-8'))
+
         return
 
 
@@ -61,10 +100,12 @@ class Thread_B(threading.Thread):
         self.name = name
 
     def run(self):
-        global temp, press, RH, wind_spd, wind_dir, heading, roll, pitch
+        global temp, press, RH, wind_spd, wind_dir, heading, roll, pitch, lat, lon, course, spd, alt
         filename = time.strftime("surface_data_%Y%m%d.sfc")
         print("Opening new file: ", filename)
         file = open(filename, 'a')
+        xdrValid = 0
+        mwvValid = 0
 
         wxt = serial.Serial('COM1', 19200, timeout=1.0)  # open wxt536 serial port
         gps = serial.Serial('COM18', 19200, timeout=1.0)  # open arduino combo device
@@ -87,6 +128,7 @@ class Thread_B(threading.Thread):
                         if msg.sentence_type == 'MWV':
                             wind_dir = msg.data[0]
                             wind_spd = msg.data[2]
+                            mwvValid = time.time()
 
                             print("\nWind Direction (deg): " + wind_dir + "  Wind Speed (m/s): " + wind_spd
                                   + "  Data Status (A=valid): " + msg.data[4])
@@ -97,14 +139,24 @@ class Thread_B(threading.Thread):
                 try:
                     if msg.talker == 'WI':
                         if msg.sentence_type == 'XDR':
-                            # print (msg.data[0],msg.data[3])
                             if (msg.data[0] == 'C' and int(msg.data[3]) == 0):  # temp, humidity, pressure message
                                 # print(line)
                                 temp = msg.data[1]
                                 RH = msg.data[5]
                                 press = msg.data[9]
-                                print("\nTemp (C): " + temp + "  RH (%): " + RH + "  Pressure (hPa): " +
-                                      press)
+                                xdrValid = time.time()
+                                print("\nTemp (C): %s  RH: %s  Pressure (hPa): %s" % (temp, RH, press))
+                                SASSI = "CLMP %f %f %s %s %s %s %s %s %s %s" % (lat, lon, course, spd, temp, temp, RH, press, wind_dir, wind_spd)
+                                print(SASSI)
+                                sassiFilename = "C:\\Users\\FOFS\\AppData\\Local\\Rasmussen Systems\\SASSI\\Outgoing\\%d.mmm" % int(time.time())
+                                print(sassiFilename)
+                                try:
+                                    sassiFile = open(sassiFilename, 'w')
+                                except:
+                                    print("Couldn't write SASSI file to " + sassiFilename)
+                                    pass
+                                sassiFile.write(SASSI)
+                                sassiFile.close()
                 except:
                     msg = pynmea2.parse(x.decode("utf-8"))
                     pass
@@ -112,6 +164,21 @@ class Thread_B(threading.Thread):
                 #print (msg.talker)
                 print(x.decode("utf-8"), end="")
                 file.write(x.decode("utf-8") + '\n')
+
+            try:
+                    if (time.time()-xdrValid ) > 120:
+                       temp = -999.0
+                       RH = -999.0
+                       press = -999.0
+            except:
+                    print ("foo!")
+
+            try:
+                    if (time.time()-mwvValid) > 15:
+                       wind_spd = -999.0
+                       wind_dir = -999.0
+            except:
+                    print ("foo2!")
 
             while (gps.in_waiting):
                 x = gps.readline()
@@ -130,7 +197,29 @@ class Thread_B(threading.Thread):
                 try:
                     msg = pynmea2.parse(x.decode("utf-8"))
                     if msg.talker == 'GP':  # decode GPRMC and GPGGA here.  update time/date variables
-                        pass
+                        if msg.sentence_type == 'RMC':
+                            if msg.data[1] == 'A':
+                                spd = msg.data[6]
+                                course = msg.data[7]
+                                lat = msg.latitude
+                                lon = msg.longitude
+                                #lat = 35
+                                #lon = -97.5
+                            else:
+                                spd = -999.0
+                                course = -999.0
+                                # generate test locations when gps is not locked
+                                #lat = 35 + random.random()
+                                #lon = -97 + random.random()
+
+                        if msg.sentence_type == 'GGA':
+                            if msg.gps_qual > 0:
+                                alt = msg.altitude
+                            else:
+                                alt = -999.0
+                                #msg = pynmea2.GGA('GP', 'GGA', (
+                                #'184353.07', '1929.045', 'S', '02410.506', 'E', '1', '04', '2.6', '100.00', 'M',
+                                #'-33.9', 'M', '', '0000')
                 except:
                     pass
 
